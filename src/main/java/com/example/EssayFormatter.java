16c574405a4e1bf6de9d885f15be70f801a03672
Omar <omarnasir77@gmail.com>
2025-01-22 10:49:50 -0600

    fixing scanner error

--- src/main/java/com/example/EssayFormatter.java
+++ src/main/java/com/example/EssayFormatter.java
@@ -1,9 +1,7 @@
 package com.example;
-import java.io.File;
+
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,140 +11,124 @@ import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
+
 public class EssayFormatter {
     public static void main(String[] args) {
-        Scanner userInput2 = new Scanner(System.in);
-        String userConfirmation = "";
-        List<String> stringlist;
+        Scanner userInput = new Scanner(System.in);
+        String userConfirmation;
+        List<String> header;
 
+        // Loop to create the header
         do {
-            stringlist = createHeader();
-            String header = String.join("\n", stringlist); 
-            System.out.println("Your header will look like this: \n\n\n" + header);
+            header = createHeader(userInput);
+            System.out.println("Your header will look like this:\n\n" + String.join("\n", header));
             System.out.println("Does this header look correct? (Yes or No?)");
-            userConfirmation = userInput2.nextLine();
+            userConfirmation = userInput.nextLine();
         } while (userConfirmation.equalsIgnoreCase("No"));
 
+        // Create Word Document
+        wordDoc(header, header.get(4), userInput);
 
-        wordDoc(stringlist, stringlist.get(4));
+        // Add References
+        references(userInput);
 
-        userInput2.close();
+        // Do not close userInput (System.in scanner)
     }
 
-    public static List<String> createHeader() {
-        Scanner userInput = new Scanner(System.in);
-        String name;
-        String professor;
-        String className;
-        String date;
-        String title;
+    public static List<String> createHeader(Scanner userInput) {
+        List<String> header = new ArrayList<>();
 
-        List<String> stringlist = new ArrayList<>();
+        System.out.println("Please submit name:");
+        header.add(userInput.nextLine());
 
-        System.out.println("Please submit name: ");
-        name = userInput.nextLine();
-        stringlist.add(name);
+        System.out.println("Please submit professor name:");
+        header.add(userInput.nextLine());
 
-        System.out.println("Please submit professor name: ");
-        professor = userInput.nextLine();
-        stringlist.add(professor);
-
-        System.out.println("Please submit class name: ");
-        className = userInput.nextLine();
-        stringlist.add(className);
+        System.out.println("Please submit class name:");
+        header.add(userInput.nextLine());
 
         System.out.println("Would you like to use today's date? (Yes or No)");
         if (userInput.nextLine().equalsIgnoreCase("Yes")) {
-            date = LocalDate.now().toString();
+            header.add(LocalDate.now().toString());
         } else {
             System.out.println("Please enter desired date:");
-            date = userInput.nextLine();
+            header.add(userInput.nextLine());
         }
-        stringlist.add(date);
 
         System.out.println("Please submit a title for your essay:");
-        title = userInput.nextLine();
-        stringlist.add(title);
+        header.add(userInput.nextLine());
 
-        return stringlist;
+        return header;
     }
 
-    public static void wordDoc(List<String> stringlist, String title) {
-            PrintWriter printer = null;
-            Scanner userInput3 = new Scanner(System.in);
+    public static void wordDoc(List<String> header, String title, Scanner userInput) {
+        System.out.println("What would you like to name your file?");
+        String fileName = userInput.nextLine() + ".docx";
 
-            stringlist.remove(4);
+        try (XWPFDocument doc = new XWPFDocument();
+             FileOutputStream output = new FileOutputStream(fileName)) {
 
-            System.out.print("What would you like to name your file?:");
-            String fileName = userInput3.nextLine();
-            fileName = fileName + ".docx";
-
-            XWPFDocument file = new XWPFDocument();
-            XWPFParagraph content = file.createParagraph();
-            XWPFRun run = content.createRun();
-
-            
-
-            System.out.print("Please enter the path of your essay file:");
-            try{
-                FileOutputStream output = new FileOutputStream(fileName);
+            // Create Header
+            for (String line : header) {
+                XWPFParagraph paragraph = doc.createParagraph();
+                paragraph.setAlignment(ParagraphAlignment.LEFT);
+                XWPFRun run = paragraph.createRun();
                 run.setFontSize(12);
                 run.setFontFamily("Times New Roman");
-                content.setSpacingBetween(2);
-
-                for(String line : stringlist){
-                    run.setText(line);
-                    if(!stringlist.get(3).equalsIgnoreCase(line))
-                        run.addBreak();
-                }
-                //file.write(output);
-
-                XWPFParagraph titleParagraph = file.createParagraph();
-                titleParagraph.setAlignment(ParagraphAlignment.CENTER);
-                XWPFRun titleRun = titleParagraph.createRun();
-                titleParagraph.setSpacingBetween(2);
-                titleRun.setFontSize(12); 
-                titleRun.setFontFamily("Times New Roman");
-                titleRun.setText(title);
-        
-                //content.setAlignment(ParagraphAlignment.CENTER);
-                //run.setText(title);
-                //run.addBreak();
-                //file.write(output);
-                //content.setAlignment(ParagraphAlignment.LEFT);
-
-
-                File usersFile = new File(userInput3.nextLine());
-                userInput3.close();
-                Scanner fileReader = new Scanner(usersFile);
-                FileWriter fw = new FileWriter(fileName);
-                printer = new PrintWriter(fw);
-                String w = fileReader.nextLine();
-
-              
-                XWPFParagraph bodyParagraph = file.createParagraph();
-                bodyParagraph.setAlignment(ParagraphAlignment.LEFT);
-                XWPFRun bodyRun = bodyParagraph.createRun();
-                bodyParagraph.setSpacingBetween(2);
-                bodyRun.setFontSize(12); 
-                bodyRun.setFontFamily("Times New Roman");
-                bodyRun.setText(w);
-
-                printer.close();
-                fileReader.close();
-               
-
-                
-                file.write(output);
-                output.close();
-            }
-            catch (IOException e) {
-                System.out.println("An error occurred while writing to the file.");
-                e.printStackTrace();
+                run.setText(line);
             }
 
+            // Create Title
+            XWPFParagraph titleParagraph = doc.createParagraph();
+            titleParagraph.setAlignment(ParagraphAlignment.CENTER);
+            XWPFRun titleRun = titleParagraph.createRun();
+            titleRun.setFontSize(12);
+            titleRun.setFontFamily("Times New Roman");
+            titleRun.setText(title);
 
+            // Save Document
+            doc.write(output);
             System.out.println("File created successfully: " + fileName);
+
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing the file.");
+            e.printStackTrace();
+        }
     }
 
+    public static void references(Scanner userInput) {
+        List<String> citations = new ArrayList<>();
+        String answer;
+
+        do {
+            System.out.println("Would you like to add citations? (Yes or No?):");
+            answer = userInput.nextLine();
+
+            if (answer.equalsIgnoreCase("Yes")) {
+                System.out.println("Please enter the author's name (Last, First):");
+                String author = userInput.nextLine();
+
+                System.out.println("Please enter the website title:");
+                String websiteTitle = userInput.nextLine();
+
+                System.out.println("Please enter the publisher:");
+                String publisher = userInput.nextLine();
+
+                System.out.println("Please enter the publish date (MM/DD/YYYY):");
+                String date = userInput.nextLine();
+
+                System.out.println("Please enter the URL:");
+                String url = userInput.nextLine();
+
+                // Format and add citation
+                String citation = author + ". " + websiteTitle + ". " + publisher + ", " + date + ". " + url;
+                citations.add(citation);
+            }
+        } while (answer.equalsIgnoreCase("Yes"));
+
+        System.out.println("\nCitations:");
+        for (String citation : citations) {
+            System.out.println(citation);
+        }
+    }
 }
